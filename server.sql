81030e4a0bb258ff6b55c4b5528696f73dd1ed0f
Adam Brown <adamrtbrown@gmail.com>
2020-07-08 12:34:23 -0600

    Updated dependencies and added DB to environment, tables to sql.

--- server.sql
+++ server.sql
@@ -3,9 +3,40 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `app`;
-CREATE TABLE `app` (
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+
+DROP TABLE IF EXISTS `apps`;
+CREATE TABLE `apps` (
   `app_id` varchar(64) NOT NULL,
   `app_secret` varchar(64) NOT NULL,
   `app_name` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `signing_keys`;
+CREATE TABLE `signing_keys` (
+  `kid` int(11) NOT NULL,
+  `private` varchar(4096) NOT NULL,
+  `public` varchar(4096) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `sign_creds`;
+CREATE TABLE `sign_creds` (
+  `provider` varchar(16) NOT NULL,
+  `kid` varchar(128) NOT NULL,
+  `key` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `uid` varchar(64) NOT NULL,
+  `app_id` varchar(64) NOT NULL,
+  `user_id` varchar(256) NOT NULL,
+  UNIQUE KEY `app_id_user_name` (`app_id`,`user_id`),
+  KEY `app_id` (`app_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
